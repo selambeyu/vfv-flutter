b99f8bf64938d8220030b28f0ca38d0666ff9928
melkam beyene <melkam3392beyu@gmail.com>
2019-12-25 08:20:04 -0800

    add signup screen

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -4,7 +4,108 @@ class SignUp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return MaterialApp(
+      title: "Vision finding village",
+      home: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.blueAccent,
+            title: Text("Vision Finding Village")
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child:TextField(
+                        scrollController: ScrollController(),
+                        decoration: InputDecoration(
+                            labelText: "Enter your name",
+                            border:OutlineInputBorder(),
+                            icon: Icon(
+                                Icons.verified_user
+                            )
+                        ),
+                      )
+                  )
+                  ,Container(
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: TextField(
+                      scrollController: ScrollController(),
+                      decoration: InputDecoration(
+                          labelText: "Enter your email",
+                          border:OutlineInputBorder(),
+                          icon: Icon(
+                              Icons.email
+                          )
+                      ),
+                    ),
+                  )
+                  ,Container(
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: TextField(
+                      scrollController: ScrollController(),
+                      decoration: InputDecoration(
+                          labelText: "Enter your password",
+                          border:OutlineInputBorder(),
+                          icon: Icon(
+                            Icons.visibility,
+
+                          )
+
+                      ),
+                    ),
+
+                  ), Container(
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: TextField(
+                      scrollController: ScrollController(),
+                      decoration: InputDecoration(
+                          labelText: "Enter your role type",
+                          border:OutlineInputBorder(),
+                          icon: Icon(
+                            Icons.supervised_user_circle,
+
+                          )
+
+                      ),
+                    ),
+
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+
+                      Container(
+                        padding: EdgeInsets.only(top: 10.0),
+                        child: MaterialButton(
+                          child: Text("Sign up",style: TextStyle(color: Colors.white60),),
+                          color: Colors.purple,
+                          onPressed: (){},
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 10.0),
+                        child: MaterialButton(
+                          child: Text("Sign up",style: TextStyle(color: Colors.white60),),
+                          color: Colors.purple,
+                          onPressed: (){},
+                        ),
+                      )
+                    ]
+                    ,
+
+                  )
+
+
+                ],
+              ),
+
+
+            )),
+      ),
+    );
   }
 
 }
