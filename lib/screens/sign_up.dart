600b4c6a86b29df4ca70a8c62582c1bb9b4571a7
melkam beyene <melkam3392beyu@gmail.com>
2019-12-27 01:55:40 -0800

    update

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vfv_app/screens/log_in.dart';
+import 'package:vfv_app/screens/professional/professional_first_screen.dart';
 import 'package:vfv_app/screens/student/student_first_screen.dart';
 import 'package:vfv_app/screens/traning_center/training_center_first_screen.dart';
 
@@ -112,31 +113,52 @@ class SignUp extends StatelessWidget{
                         ,
 
                       ),
-                      Container(
-                        height: 50.0,
-                        padding: EdgeInsets.only(top: 10.0),
-                        child: MaterialButton(
-                          child: Text("Training",style: TextStyle(color: Colors.white60),),
-                          color: Colors.purple,
-                          onPressed: (){
-                            Navigator.push(context, MaterialPageRoute(builder: (context)=>TrainingCetersFirstScreen()));
-                          },
-                        ),
-                      ),
-                      Container(
-                        height: 50.0,
-                        padding: EdgeInsets.only(top: 10.0),
-                        child: MaterialButton(
-                          child: Text("Student",style: TextStyle(color: Colors.white60),),
-                          color: Colors.purple,
-                          onPressed: (){
-                            Navigator.push(context, MaterialPageRoute(builder: (context)=>StudentFirstScreen()));
-                          },
-                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Container(
+                            height: 50.0,
+                            padding: EdgeInsets.only(top: 10.0),
+                            child: MaterialButton(
+                              child: Text("Training",style: TextStyle(color: Colors.white60),),
+                              color: Colors.purple,
+                              onPressed: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context)=>TrainingCetersFirstScreen()));
+                              },
+                            ),
+                          ),
+                          Container(
+                            height: 50.0,
+                            padding: EdgeInsets.only(top: 10.0),
+                            child: MaterialButton(
+                              child: Text("Student",style: TextStyle(color: Colors.white60),),
+                              color: Colors.purple,
+                              onPressed: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context)=>StudentFirstScreen()));
+                              },
+                            ),
+                          ),
+                          Container(
+                            height: 50.0,
+                            padding: EdgeInsets.only(top: 10.0),
+                            child: MaterialButton(
+                              child: Text("Professional",style: TextStyle(color: Colors.white60),),
+                              color: Colors.purple,
+                              onPressed: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context)=>ProfessionalFirstScreen()));
+                              },
+                            ),
+                          ),
+                        ],
                       )
 
 
+
+
+
+
                     ],
+
 
                   ),
 
