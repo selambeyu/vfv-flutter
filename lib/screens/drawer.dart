dce3f916ab80bbd61999b61787aa41d60162ba10
melkam beyene <melkam3392beyu@gmail.com>
2019-12-26 23:25:45 -0800

    add drawer.dart

---
+++ lib/screens/drawer.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class DrawerWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Drawer(
+      child:
+      ListView(
+        children: <Widget>[
+          DrawerHeader(
+
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: <Color>[
+                    Colors.deepOrange,
+                    Colors.orangeAccent
+                  ]
+              ),
+
+            ),
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/images/logo.png',scale: 1.7),
+                  Text("Drawer")
+                ],
+              ),
+            ),
+          ),
+          CustomListTile(Icons.person,"User",()=>{}),
+          CustomListTile(Icons.email,"Email",()=>{}),
+          CustomListTile(Icons.verified_user,"Contact",()=>{}),
+
+        ],
+      ),
+    );
+  }
+
+}
+
+
+class CustomListTile extends StatelessWidget{
+  IconData iconData;
+  Function onTap;
+  String text;
+
+  CustomListTile(this.iconData,this.text,this.onTap);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+        padding: EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+        child:InkWell(
+            splashColor: Colors.orangeAccent,
+            onTap: (){},
+            child:Container(
+              height: 40.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row( children: <Widget>[
+
+                    Icon(iconData),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(text,style: TextStyle(fontSize: 16.0),),
+                    )
+
+                  ],),
+
+                  Icon(Icons.arrow_right)
+                ],
+              ),)
+
+        )
+
+    );
+
+  }
+
+}
+
--- lib/screens/student/student_first_screen.dart
+++ lib/screens/student/student_first_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vfv_app/screens/drawer.dart';
 
 class StudentFirstScreen extends StatelessWidget{
   @override
@@ -29,8 +30,7 @@ class StudentFirstScreen extends StatelessWidget{
                 ),
               ),
             ),   Card(
-
-                  child: Container(
+                    child: Container(
                     child: Text("This is card 1"),
                     decoration: BoxDecoration(
                         color: Colors.purple
@@ -43,76 +43,9 @@ class StudentFirstScreen extends StatelessWidget{
           ],
         ),
       ) ,
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: <Color>[
-                    Colors.deepOrange,
-                    Colors.orangeAccent
-                  ]
-                ),
-
-              ),
-              child: Container(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/images/logo.png',scale: 1.7),
-                    Text("Drawer")
-                  ],
-                ),
-              ),
-            ),
-            CustomListTile(),
-            CustomListTile(),
-            CustomListTile(),
-            CustomListTile(),
-            CustomListTile(),
-
-          ],
-        ),
-      ),
+      drawer: DrawerWidget(),
 
     );
-  }
-
-}
-
-class CustomListTile extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Padding(
-      padding: EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
-        child:InkWell(
-          splashColor: Colors.orangeAccent,
-          onTap: (){},
-          child:Container(
-            height: 40.0,
-            child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Row( children: <Widget>[
-
-                Icon(Icons.person),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text("User Name",style: TextStyle(fontSize: 16.0),),
-                )
-
-              ],),
-
-              Icon(Icons.arrow_right)
-            ],
-          ),)
-
-        )
-
-    );
-
   }
 
 }
